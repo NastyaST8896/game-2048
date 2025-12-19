d90f6bff20a763f18d0450dca01dfa3104f56502
Anastasia Triapochkina <me@trjapochkina.ru>
2025-12-19 18:06:34 +0300

    add start board2

--- script.js
+++ script.js
@@ -1,58 +1,66 @@
 const board = document.querySelector("#board");
 
-const cellСontents = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+const cellContents = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
 function getNumber() {
     return Math.round(Math.random() * 100) >= 10 ? 2 : 4;
 }
 
 function getCell(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function createNewCellsBoard() {
+const number = [];
 
-    const number = [];
-
-    for(let i = 0; i < 2; i++) {
+  for(let i = 0; i < 2; i++) {
         number.push( { num: getNumber(), use: false } );
     }
-   
-    const cellNumber = [];
 
-    function addCellNumber() {
-        for(let i = 0; i < 2; i++) {
+const cellNumber = [];
+
+function addCellNumber() {
+    for(let i = 0; i < 2; i++) {
             cellNumber.push(getCell(1,16));
-        }
-
-        if(cellNumber[0] === cellNumber[1]) {
-            addCellNumber()
-        }
     }
+
+    if(cellNumber[0] === cellNumber[1]) {
+            addCellNumber();
+    } 
+}
     
-    addCellNumber()
+addCellNumber();
+
+function addFirstNumbers() {
+
+    cellContents.forEach((item,index) => {
+        cellNumber.forEach((cell) => {
+            if(index+1 === cell) {
+                let freeNumber = number.find((item) => {
+                return item.use === false;
+            });
+
+            cellContents[index] = freeNumber.num
+            item.use = !item.use;
+            }
+        })
+    })
+}
+
+addFirstNumbers()
+
+function renderBoard() {
 
     for(let i = 1; i < 17; i++) {
         let cell = document.createElement('div');
-        cell.classList = 'cell'
-        cell.id = i
+        cell.classList = 'cell';
+        cell.id = i;
 
-
-
-        if(cellNumber.includes(i) && !cell.textContent) {
-            let freeNumber = number.find((item) => {
-                return item.use === false
-            });
-
-            cellСontents[i-1] = freeNumber.num
-
-            cell.textContent = freeNumber.num;
-            freeNumber.use = !freeNumber.use
+        if(cellContents[i-1] !== 0) {
+            cell.textContent = cellContents[i-1];
         }
-
+        
         board.append(cell);
     }
 }
 
-createNewCellsBoard();
-console.log(cellСontents)
+renderBoard()
